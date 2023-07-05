90d08ccba2b528d4d5f4fdbad9ce674d0ef33758
remix <remix7483@pm.me>
2023-07-05 16:50:25 +0200

    rename stuff

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::str::FromStr;
 #[allow(non_camel_case_types)]
 #[rustfmt::skip]
 #[derive(Debug, PartialEq)]
-pub enum RPCErrorCode {
+pub enum Error {
     // General application defined errors
     RPC_MISC_ERROR,                // std::exception thrown in command handling
     RPC_TYPE_ERROR(TypeError),     // Unexpected type was passed as parameter
@@ -58,57 +58,62 @@ pub enum RPCErrorCode {
     RPC_UNKOWN_ERROR(i32, String),
 }
 
-impl FromStr for RPCErrorCode {
+impl FromStr for Error {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let regex = Regex::new(r#"\{"code": (-?\d+), "message": "(.*?)"\}"#).unwrap();
         let captures = regex.captures(&s).ok_or(())?;
-    
-        let code: i32 = captures.get(1).ok_or(())?.as_str().parse().map_err(|_| ())?;
+
+        let code: i32 = captures
+            .get(1)
+            .ok_or(())?
+            .as_str()
+            .parse()
+            .map_err(|_| ())?;
         let message = captures.get(2).ok_or(())?.as_str().to_string();
-    
+
         match (code, &message) {
             // General application defined errors
-            (-3, m) => Ok(RPCErrorCode::RPC_TYPE_ERROR(m.parse().unwrap())),
-            (-5, _) => Ok(RPCErrorCode::RPC_INVALID_ADDRESS_OR_KEY),
-            (-7, _) => Ok(RPCErrorCode::RPC_OUT_OF_MEMORY),
-            (-8, _) => Ok(RPCErrorCode::RPC_INVALID_PARAMETER),
-            (-20, _) => Ok(RPCErrorCode::RPC_DATABASE_ERROR),
-            (-22, _) => Ok(RPCErrorCode::RPC_DESERIALIZATION_ERROR),
-            (-25, m) => Ok(RPCErrorCode::RPC_VERIFY_ERROR(m.parse().unwrap())),
-            (-26, _) => Ok(RPCErrorCode::RPC_VERIFY_REJECTED),
-            (-27, _) => Ok(RPCErrorCode::RPC_VERIFY_ALREADY_IN_CHAIN),
-            (-28, _) => Ok(RPCErrorCode::RPC_IN_WARMUP),
-            (-32, _) => Ok(RPCErrorCode::RPC_METHOD_DEPRECATED),
+            (-3, m) => Ok(Error::RPC_TYPE_ERROR(m.parse().unwrap())),
+            (-5, _) => Ok(Error::RPC_INVALID_ADDRESS_OR_KEY),
+            (-7, _) => Ok(Error::RPC_OUT_OF_MEMORY),
+            (-8, _) => Ok(Error::RPC_INVALID_PARAMETER),
+            (-20, _) => Ok(Error::RPC_DATABASE_ERROR),
+            (-22, _) => Ok(Error::RPC_DESERIALIZATION_ERROR),
+            (-25, m) => Ok(Error::RPC_VERIFY_ERROR(m.parse().unwrap())),
+            (-26, _) => Ok(Error::RPC_VERIFY_REJECTED),
+            (-27, _) => Ok(Error::RPC_VERIFY_ALREADY_IN_CHAIN),
+            (-28, _) => Ok(Error::RPC_IN_WARMUP),
+            (-32, _) => Ok(Error::RPC_METHOD_DEPRECATED),
 
             // P2P client errors
-            (-9, _) => Ok(RPCErrorCode::RPC_CLIENT_NOT_CONNECTED),
-            (-10, _) => Ok(RPCErrorCode::RPC_CLIENT_IN_INITIAL_DOWNLOAD),
-            (-23, _) => Ok(RPCErrorCode::RPC_CLIENT_NODE_ALREADY_ADDED),
-            (-24, _) => Ok(RPCErrorCode::RPC_CLIENT_NODE_NOT_ADDED),
-            (-29, _) => Ok(RPCErrorCode::RPC_CLIENT_NODE_NOT_CONNECTED),
-            (-30, _) => Ok(RPCErrorCode::RPC_CLIENT_INVALID_IP_OR_SUBNET),
-            (-31, _) => Ok(RPCErrorCode::RPC_CLIENT_P2P_DISABLED),
-            (-34, _) => Ok(RPCErrorCode::RPC_CLIENT_NODE_CAPACITY_REACHED),
+            (-9, _) => Ok(Error::RPC_CLIENT_NOT_CONNECTED),
+            (-10, _) => Ok(Error::RPC_CLIENT_IN_INITIAL_DOWNLOAD),
+            (-23, _) => Ok(Error::RPC_CLIENT_NODE_ALREADY_ADDED),
+            (-24, _) => Ok(Error::RPC_CLIENT_NODE_NOT_ADDED),
+            (-29, _) => Ok(Error::RPC_CLIENT_NODE_NOT_CONNECTED),
+            (-30, _) => Ok(Error::RPC_CLIENT_INVALID_IP_OR_SUBNET),
+            (-31, _) => Ok(Error::RPC_CLIENT_P2P_DISABLED),
+            (-34, _) => Ok(Error::RPC_CLIENT_NODE_CAPACITY_REACHED),
 
             // Wallet errors
-            (-4, _) => Ok(RPCErrorCode::RPC_WALLET_ERROR),
-            (-6, _) => Ok(RPCErrorCode::RPC_WALLET_INSUFFICIENT_FUNDS),
-            (-11, _) => Ok(RPCErrorCode::RPC_WALLET_INVALID_LABEL_NAME),
-            (-12, _) => Ok(RPCErrorCode::RPC_WALLET_KEYPOOL_RAN_OUT),
-            (-13, _) => Ok(RPCErrorCode::RPC_WALLET_UNLOCK_NEEDED),
-            (-14, _) => Ok(RPCErrorCode::RPC_WALLET_PASSPHRASE_INCORRECT),
-            (-15, _) => Ok(RPCErrorCode::RPC_WALLET_WRONG_ENC_STATE),
-            (-16, _) => Ok(RPCErrorCode::RPC_WALLET_ENCRYPTION_FAILED),
-            (-17, _) => Ok(RPCErrorCode::RPC_WALLET_ALREADY_UNLOCKED),
-            (-18, _) => Ok(RPCErrorCode::RPC_WALLET_NOT_FOUND),
-            (-19, _) => Ok(RPCErrorCode::RPC_WALLET_NOT_SPECIFIED),
-            (-35, _) => Ok(RPCErrorCode::RPC_WALLET_ALREADY_LOADED),
-            (-36, _) => Ok(RPCErrorCode::RPC_WALLET_ALREADY_EXISTS),
+            (-4, _) => Ok(Error::RPC_WALLET_ERROR),
+            (-6, _) => Ok(Error::RPC_WALLET_INSUFFICIENT_FUNDS),
+            (-11, _) => Ok(Error::RPC_WALLET_INVALID_LABEL_NAME),
+            (-12, _) => Ok(Error::RPC_WALLET_KEYPOOL_RAN_OUT),
+            (-13, _) => Ok(Error::RPC_WALLET_UNLOCK_NEEDED),
+            (-14, _) => Ok(Error::RPC_WALLET_PASSPHRASE_INCORRECT),
+            (-15, _) => Ok(Error::RPC_WALLET_WRONG_ENC_STATE),
+            (-16, _) => Ok(Error::RPC_WALLET_ENCRYPTION_FAILED),
+            (-17, _) => Ok(Error::RPC_WALLET_ALREADY_UNLOCKED),
+            (-18, _) => Ok(Error::RPC_WALLET_NOT_FOUND),
+            (-19, _) => Ok(Error::RPC_WALLET_NOT_SPECIFIED),
+            (-35, _) => Ok(Error::RPC_WALLET_ALREADY_LOADED),
+            (-36, _) => Ok(Error::RPC_WALLET_ALREADY_EXISTS),
 
             // Unknown Error
-            _ => Ok(RPCErrorCode::RPC_UNKOWN_ERROR(code, message)),
+            _ => Ok(Error::RPC_UNKOWN_ERROR(code, message)),
         }
     }
 }
