0ae0788e033893876ca2d987fc4b81c8e3b22a9b
remix <remix7483@pm.me>
2023-07-10 10:11:35 +0200

    add start of string (^) and end of string ($) symbols to patterns

--- src/general_errors.rs
+++ src/general_errors.rs
@@ -5,60 +5,60 @@ pub enum TypeError {
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/signmessage.cpp#L49
     // https://github.com/bitcoin/bitcoin/blob/master/src/wallet/rpc/signmessage.cpp#L55
     // https://github.com/bitcoin/bitcoin/blob/master/src/wallet/rpc/backup.cpp#L679
-    #[patterns("Address does not refer to key", "Address does not refer to a key")]
+    #[patterns("^Address does not refer to key$", "^Address does not refer to a key$")]
     AddressNoKey,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/signmessage.cpp#L51
-    #[patterns("Malformed base64 encoding")]
+    #[patterns("^Malformed base64 encoding$")]
     MalformedBase64,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L45
-    #[patterns("JSON value of type (.*)")]
+    #[patterns("^JSON value of type (.*)")]
     WrongJsonType(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L55
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L55
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L55
-    #[patterns("Unexpected key (.*)")]
+    #[patterns("^Unexpected key (.*)")]
     UnexpectedKey(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L64
-    #[patterns("Amount is not a number or string")]
+    #[patterns("^Amount is not a number or string$")]
     AmountNotNumberOrString,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L67
-    #[patterns("Invalid amount")]
+    #[patterns("^Invalid amount$")]
     AmountInvalid,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L69
-    #[patterns("Amount out of range")]
+    #[patterns("^Amount out of range$")]
     AmountOutOfRange,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L589
-    #[patterns("Wrong type passed:\n(.*)")]
+    #[patterns("^Wrong type passed:\n(.*)")]
     WrongPassed(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/mining.cpp#L664
-    #[patterns("Missing data String key for proposal")]
+    #[patterns("^Missing data String key for proposal$")]
     MissingKeyForProposal,
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/wallet/rpc/backup.cpp#L1246
-    #[patterns("Expected number or \"now\" timestamp value for key. got type (.*)")]
+    #[patterns("^Expected number or \"now\" timestamp value for key. got type (.*)")]
     WrongTimestamp(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/wallet/rpc/backup.cpp#L1248
-    #[patterns("Missing required timestamp field for key")]
+    #[patterns("^Missing required timestamp field for key$")]
     MissingTimestamp,
 
     // // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/rawtransaction_util.cpp#L308
-    #[patterns("Missing amount for (.*)")]
+    #[patterns("^Missing amount for (.*)")]
     MissingAmountForCoins(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/util.cpp#L42
     // TODO: The capture can be an arbitrary string. We can not distinguish it from
     // MissingKeyForProposal, MissingTimestamp or MissingAmountForCoins
     // This only works because the variants are checked in order!
-    #[patterns("Missing (.*)")]
+    #[patterns("^Missing (.*)")]
     Missing(String),
 }
 
@@ -166,15 +166,15 @@ pub enum DeserializationError {
 #[derive(Clone, Debug, PartialEq, EnumError)]
 pub enum VerifyError {
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/mining.cpp#L379
-    #[patterns("TestBlockValidity failed: (.*)")]
+    #[patterns("^TestBlockValidity failed: (.*)")]
     BlockValidityFailed(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/mining.cpp#L1064
-    #[patterns(r"Must submit previous header \((.*)\) first")]
+    #[patterns(r"^Must submit previous header \((.*)\) first$")]
     PreviousHeaderMissing(String),
 
     // https://github.com/bitcoin/bitcoin/blob/master/src/rpc/rawtransaction.cpp#L697
-    #[patterns("Input not found or already spent")]
+    #[patterns("^Input not found or already spent$")]
     MissingOrSpend,
 
     // No Pattern
