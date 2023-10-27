0c8e3e31f3089da336d358486472d911b934dc86
remix <remix7531@pm.me>
2023-10-27 18:25:24 +0200

    added MISC_ERROR

--- src/general_errors.rs
+++ src/general_errors.rs
@@ -2,6 +2,142 @@ use proc_macro_magic::EnumError;
 
 #[derive(Clone, Debug, PartialEq, EnumError)]
 pub enum TypeError {
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L459
+    #[patterns("^Block header missing$")]
+    BlockHeaderMissing,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L465
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L586
+    #[patterns(
+        "^In prune mode, only blocks that the node has already synced previously can be fetched from a peer$",
+        "^Block not available (pruned data)$"
+    )]
+    BlockUnavailablePrunedNode,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L470
+    #[patterns("^Block already downloaded$")]
+    BlockAlreadyDownloaded,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L594
+    #[patterns("^Block not found on disk$")]
+    BlockNotFound,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L610
+    #[patterns("^Undo data not available (pruned data)$")]
+    UndoUnavailablePrunedNode,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L615
+    #[patterns("^Can't read undo data from disk$")]
+    UndoNotFound,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L781
+    #[patterns("^Cannot prune blocks because node is not in prune mode.$")]
+    PruneForbidden,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L807
+    #[patterns("^Blockchain is too short for pruning.$")]
+    ChainToShort,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2181
+    #[patterns("^scanobjects argument is required for the start action$")]
+    ArgumentMissing,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2383
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2523
+    #[patterns("^Index is not enabled for filtertype (.*)")]
+    IndexFilterForbidden(String),
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2400
+    #[patterns("^Invalid start_height$")]
+    InvalidStartHeight,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2406
+    #[patterns("^Invalid stop_height$")]
+    InvalidStopHeight,
+
+    // TODO: Refactoring Core might be a good idea here
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L2551
+    #[patterns("^Filter not found. Block filters are still in the process of being indexed.$")]
+    IndexNotReady,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/external_signer.cpp#L43
+    #[patterns("^Error: restart bitcoind with -signer=<cmd>$")]
+    SignerArgMissing,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/mempool.cpp#L331
+    #[patterns("^Transaction is not in mempool$")]
+    TxNotInMempool,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/mempool.cpp#L742
+    #[patterns("^The mempool was not loaded yet$")]
+    MempoolNotLoaded,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/mempool.cpp#L748
+    #[patterns("^Unable to dump mempool to disk$")]
+    CanNotDumpMempool,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/mining.cpp#L387
+    #[patterns("^Failed to make block.$")]
+    BlockGenerationFailed,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/output_script.cpp#L293
+    #[patterns("^Unexpected empty result$")]
+    EmptyAddresses,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/rawtransaction.cpp#L286
+    #[patterns("^Block not available$")]
+    BlockNotFound,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/addresses.cpp#L781
+    #[patterns("^Failed to display address$")]
+    FailedToDisplayAddress,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L91
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L1405
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L1699
+    #[patterns("^Rescan aborted by user.$")]
+    RescanUserAborted,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L1226
+    #[patterns("^Missing required fields$")]
+    MissingFields,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L1425
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L1723
+    #[patterns("^Rescan failed for key with creation timestamp (.*). There was an error reading a \
+                block from time (.*), which is after or within (.*) seconds of key creation, and \
+                could contain transactions pertaining to the key. As a result, transactions \
+                and coins using this key may not appear in the wallet. This error could be \
+                caused by pruning or data corruption (see bitcoind log for details) and could \
+                be dealt with by downloading and rescanning the relevant blocks (see -reindex \
+                option and rescanblockchain RPC).$")]
+    RescanFailed(String, String, String),
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/transactions.cpp#L907
+    #[patterns("^Can't rescan beyond pruned data. Use RPC call getblockchaininfo to determine your pruned height.$")]
+    ScanBeyondPrune,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/transactions.cpp#L919
+    #[patterns("^Rescan failed. Potentially corrupted data files.$")]
+    RescanFailed,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/transactions.cpp#L921
+    #[patterns("^Rescan aborted.$")]
+    RescanAborted,
+    
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/wallet.cpp#L487
+    #[patterns("^Requested wallet already unloaded$")]
+    WalletUnloaded,
+
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/blockchain.cpp#L474
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/external_signer.cpp#L56
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/server.cpp#510
+    // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/spend.cpp#1125
+    Generic(String),
+}
+
+#[derive(Clone, Debug, PartialEq, EnumError)]
+pub enum TypeError {
     // https://github.com/bitcoin/bitcoin/blob/v25.0/src/rpc/signmessage.cpp#L49
     // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/signmessage.cpp#L55
     // https://github.com/bitcoin/bitcoin/blob/v25.0/src/wallet/rpc/backup.cpp#L675
